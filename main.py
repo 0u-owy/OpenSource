a3b118a37d484b244ccad31c826b3d2560fdac22
0u-owy <youngyou0926@gmail.com>
2024-12-02 15:36:21 +0900

    제곱근 기능 추가

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 def main():
     print("----------------------------")
     print("계산기 프로그램 입니다.")
@@ -12,13 +13,14 @@ def main():
         print("3. 곱하기")
         print("4. 나누기")
         print("5. 지수")  # 지수 기능 추가
+        print("6. 제곱근")  # 제곱근 기능 추가
 
         choice = input("번호 : ")
 
         if choice == "quit":
             print("종료")
             break
-        elif choice in ["1", "2", "3", "4","5"]:
+        elif choice in ["1", "2", "3", "4","5","6"]:
             x = float(input("첫 번째 숫자 입력: "))
             y = float(input("두 번째 숫자 입력: "))
             if choice == 1:
@@ -34,6 +36,11 @@ def main():
                     print("0으로 나눌 수 없습니다.") # 0으로 나누면 오류 메시지
             elif choice == "5":  # 지수 (x^y)
                 print("결과:", x ** y)  # x^y 계산
+            elif choice == 6:  # 제곱근
+                if x >= 0:  # 음수에 대한 제곱근은 계산 불가
+                    print("결과:", math.sqrt(x))  # 제곱근 계산
+                else:
+                    print("음수에 대한 제곱근은 계산할 수 없습니다.")
         else:
             print("잘못된 입력.")
 
