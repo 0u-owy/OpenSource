47718f64ebe2f224ad9bffbbb97b4d35d0cf7bcc
0u-owy <youngyou0926@gmail.com>
2024-12-05 14:10:39 +0900

    최종 수정

--- main.py
+++ main.py
@@ -22,21 +22,22 @@ def main():
             break
         elif choice in ["1", "2", "3", "4","5","6"]:
             x = float(input("첫 번째 숫자 입력: "))
-            y = float(input("두 번째 숫자 입력: "))
-            if choice == 1:
+            if choice in ["1","2","3","4","5"]: #두번째 숫자가 필요한 경우
+                y = float(input("두 번째 숫자 입력: "))
+            if choice == "1":
                 print("결과:")
-            elif choice == 2:
+            elif choice == "2":
                 print("결과:")
-            elif choice == 3:
+            elif choice == "3":
                 print("결과: ", x*y)
-            elif choice == 4:
+            elif choice == "4":
                 if y != 0:  # 0으로 나누는 경우를 방지
                     print("결과:", x/y)
                 else:
                     print("0으로 나눌 수 없습니다.") # 0으로 나누면 오류 메시지
-            elif choice == 5:  # 지수 (x^y)
+            elif choice == "5":  # 지수 (x^y)
                 print("결과:", x ** y)  # x^y 계산
-            elif choice == 6:  # 제곱근
+            elif choice == "6":  # 제곱근
                 if x >= 0:  # 음수에 대한 제곱근은 계산 불가
                     print("결과:", math.sqrt(x))  # 제곱근 계산
                 else:
